2ef32d58fed8d5572b456fda7c16eaf6f1e5dc4e
OBASTIAS <dj.osckaar@gmail.com>
2021-06-18 01:12:28 -0400

    modificacion puts

--- mayor_de_3.rb
+++ mayor_de_3.rb
@@ -3,9 +3,9 @@ b = ARGV[1].to_i # Ingresa segundo número
 c = ARGV[2].to_i # Ingresa tercer número
 
 if a >= b && a >= c # si a es mayor o igual que b y a su vez a es mayor o igual que c
-    puts "#{a} es el mayor" # a es el mayor
+    puts "#{a}" # a es el mayor
 elsif b >= c #caso contrario, analizar si b es mayor o igual que c
-    puts "#{b} es el mayor" # de ser asi, b es el mayor
+    puts "#{b}" # de ser asi, b es el mayor
 else # caso contrario, c es el mayor
-    puts "#{c} es el mayor" 
+    puts "#{c}" 
 end
